b76c4be4f039733d1ba44e5d7ed4991a0e2077ef
Bear21 <bear@8bitbear.com>
2019-07-07 12:33:32 +1000

    Added Cache test

--- cthreadping.cpp
+++ cthreadping.cpp
@@ -1,15 +1,28 @@
 // cthreadping.cpp : Defines the entry point for the console application.
+// For testing performance between processors, cores and threads on the same system.
+// Tests memory bandwidth at varying sizes and thread to thread latency
 //
+//  Copyright(C) 2019  Stephen Wheeler - 8bitbear.com - https://github.com/Bear21
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see<https://www.gnu.org/licenses/>.
 
 #include "stdafx.h"
-#include <windows.h>
-#include "timecounter.h"
-#include "Mmsystem.h"
-#include <float.h>
-#include <chrono>
-#include <thread>
+#include "cthreadping.h"
 
 int Nthread = 0;
+
+HANDLE mainThread;
 
 int pingCount = 10000000;
 int pingMult = 8;
@@ -124,7 +137,7 @@ int main()
    SetupThreadPriority();
    SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
    HANDLE thr = GetCurrentThread();
-   
+   mainThread = thr;
 
    DWORD_PTR dwSystemAffinity, dwProcessAffinity;
    if( GetProcessAffinityMask( GetCurrentProcess(),
@@ -149,28 +162,10 @@ int main()
       TimePast tp = TimePast();
       while (tp.Peek() <= 1.05f);
    }*/
-   LONG lock = 0;
-   printf("core");
-   for (int i = 0; i < Nthread; i++)
-   {
-      printf(", %d", i);
-   }
-   for (int i = Nthread - 1; i >= 0; i--) {
-      printf("\n%d", i);
-      for (int j = 0; j < Nthread; j++)
-      {
-         if (i == j)
-         {
-            printf(", 0.0");
-            continue;
-         }
-         //printf("Testing %d against %d:\n   ", i, j);
-         std::thread pongthr(SlavePong, i, &lock);
-         Ping(j, &lock);
-         pongthr.join();
-         lock = 0;
-      }
-   }
+   
+   PingTest();
+   
+   CacheTest();
    puts("");
    system("pause");
    TimePast tp;
@@ -230,3 +225,74 @@ int main()
    return 0;
 }
 
+void PingTest()
+{
+   LONG lock = 0;
+   printf("\ncore to core latency(ns)");
+   for (int i = 0; i < Nthread; i++)
+   {
+      printf(", %d", i);
+   }
+   for (int i = Nthread - 1; i >= 0; i--) {
+      printf("\n%d", i);
+      for (int j = 0; j < Nthread; j++)
+      {
+         if (i == j)
+         {
+            printf(", 0.0");
+            continue;
+         }
+         //printf("Testing %d against %d:\n   ", i, j);
+         std::thread pongthr(SlavePong, i, &lock);
+         Ping(j, &lock);
+         pongthr.join();
+         lock = 0;
+      }
+   }
+}
+
+void CacheTest()
+{
+#define CACHETESTSTARTSIZE 1024*16
+#define CACHETESTENDSIZE 1024*1024*256
+   char* data = new char[CACHETESTENDSIZE];
+   TimePast tp;
+   for (size_t loop = CACHETESTSTARTSIZE; loop <= CACHETESTENDSIZE; loop *= 2)
+   {
+      int cachePingCount = (int)(1.f / (float)((float)loop / ((float)CACHETESTENDSIZE))) * 4;
+      printf("\nTesting cache size %dKB (GB/s)", loop / 1024);
+      for (int i = 0; i < Nthread; i++)
+      {
+         printf(", %d", i);
+      }
+      for (int i = Nthread - 1; i >= 0; i--) {
+         printf("\n%d", i);
+         
+         for (int j = 0; j < Nthread; j++)
+         {
+            float result = 0;
+            for (int average = 0; average < cachePingCount; average++) {
+               SetAffinity(mainThread, i);
+               memset(data, i, loop);
+               Prepare(mainThread, j);
+
+               timeBeginPeriod(0);
+               tp.Reset();
+               // 32 is mid cache line, assuming 64byte cache lines.
+               for (int checkIt = 32; checkIt <= loop; checkIt += 64)
+               {
+                  if (data[checkIt] != i) {
+                     printf("\nWTF fail? run memtest86+\n");
+                  }
+               }
+               result += tp.Check();
+               timeEndPeriod(0);
+            }
+            result /= cachePingCount;
+            result = (float)(loop) / (1024.f*1024.f*1024.f) / result;
+            printf(", %f", result);
+         }
+      }
+   }
+   delete[] data;
+}
---
+++ cthreadping.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windows.h>
+#include "timecounter.h"
+#include "Mmsystem.h"
+#include <float.h>
+#include <chrono>
+#include <thread>
+
+int main();
+void PingTest();
+void CacheTest();
